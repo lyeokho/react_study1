37d6efd98c6b074c1479c92b3cb7e0f7166be2ae
lyeokho <rkdfurgh@naver.com>
2025-07-16 16:23:08 +0900

    게시판초기

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+        //컨포넌트명
 function App() {
-  const [count, setCount] = useState(0)
+  let title='게시판';  //화면 변경 시 제목 변경되게 
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+    <div className='APP'>
+      <div className='nav'>
+        <h2>{title}</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
+
   )
 }
 
